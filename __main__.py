f171509bd73ad60f121fe59b9e9d1a10a522dbd9
linden <pixilreal@gmail.com>
2026-06-02 17:35:24 +0200

    26.5

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ from urllib.request import urlretrieve
 match sys.argv[1]:
     case "get":
         if len(sys.argv) == 2:
-            print("Usage: python nitrogen get <publication> [release (latest by default)]")
+            print("Usage: python Nitrogen get <publication> [release (latest by default)]")
         urlretrieve(f"https://github.com/Wednesware/{sys.argv[2].capitalize()}/releases/{sys.argv[3] if len(sys.argv) > 3 else 'latest'}/download/{sys.argv[2].lower()}.zip", f"{sys.argv[2].lower()}.zip")
         with zipfile.ZipFile(f"{sys.argv[2].lower()}.zip", "r") as zip_ref:
             zip_ref.extractall(f"{sys.argv[2].lower()}-repo")
@@ -67,11 +67,11 @@ match sys.argv[1]:
         with open(os.path.join(os.path.dirname(__file__), "README.md")) as file:
             print(file.read())
     case "help":
-        print("Usage: python nitrogen <command> [args]")
+        print("Usage: python Nitrogen <command> [args]")
         print("Commands:")
         print("  get <publication> [release (latest by default)] - Download a Wednesware publication from GitHub")
         print("  readme - Show the README file")
         print("  help - Show this help message")
     case _:
         print(f"Unknown command: {sys.argv[1]}")
-        print("Use 'python nitrogen help' for a list of commands.")
+        print("Use 'python Nitrogen help' for a list of commands.")
