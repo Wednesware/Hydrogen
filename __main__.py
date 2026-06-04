4acc1228b5b20ce82629a52588ff9040fbaf3386
linden 💛🤍💜🖤 <146763980+heylinden@users.noreply.github.com>
2026-06-04 11:17:23 +0200

    26.12

--- __main__.py
+++ __main__.py
@@ -2,14 +2,14 @@ import sys, zipfile, shutil, os, urllib.error
 from urllib.request import urlretrieve
 
 if len(sys.argv) == 1:
-    print("Usage: python Nitrogen <command> [args]")
-    print("Use 'python Nitrogen help' for a list of commands.")
+    print("Usage: python nitrogen <command> [args]")
+    print("Use 'python nitrogen help' for a list of commands.")
     sys.exit(0)
 
 match sys.argv[1]:
     case "get":
         if len(sys.argv) == 2:
-            print("Usage: python Nitrogen get <publication> [release (latest by default)]")
+            print("Usage: python nitrogen get <publication> [release (latest by default)]")
         try:
             urlretrieve(f"https://github.com/Wednesware/{sys.argv[2].capitalize()}/releases/{sys.argv[3] if len(sys.argv) > 3 else 'latest'}/download/{sys.argv[2].lower()}.zip", f"{sys.argv[2].lower()}.zip")
         except urllib.error.HTTPError:
@@ -22,16 +22,16 @@ match sys.argv[1]:
         shutil.move(f"{sys.argv[2].lower()}-repo/{next(os.scandir(f'{sys.argv[2].lower()}-repo')).name}/{sys.argv[2].lower()}", f"{sys.argv[2].lower()}")
         shutil.rmtree(f"{sys.argv[2].lower()}-repo")
         os.remove(f"{sys.argv[2].lower()}.zip")
-        print("\033[92mInstallation complete! Run 'rm -rf Nitrogen' to remove this installer.\033[0m")
+        print("\033[92mInstallation complete! Run 'rm -rf nitrogen' to remove this installer.\033[0m")
     case "readme":
         with open(os.path.join(os.path.dirname(__file__), "README.md")) as file:
             print(file.read())
     case "help":
-        print("Usage: python Nitrogen <command> [args]")
+        print("Usage: python nitrogen <command> [args]")
         print("Commands:")
         print("  get <publication> [release (latest by default)] - Download a Wednesware publication from GitHub")
         print("  readme - Show the README file")
         print("  help - Show this help message")
     case _:
         print(f"Unknown command: {sys.argv[1]}")
-        print("Use 'python Nitrogen help' for a list of commands.")
+        print("Use 'python nitrogen help' for a list of commands.")
