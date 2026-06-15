37bc3b7ecae6677302d9aab2ef2e56efd5dcc94d
linden <pixilreal@gmail.com>
2026-06-15 19:10:43 +0200

    26.23

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wwn",
-    version="26.22",
+    version="26.23",
     py_modules=[],
     entry_points={
         "console_scripts": [
