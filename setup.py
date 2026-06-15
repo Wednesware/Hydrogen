4fc657d4699b9912be51aff5903458ccf9e27016
linden <pixilreal@gmail.com>
2026-06-15 15:20:40 +0200

    26.19

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nitro",
+    version="26.19",
+    author="Wednesware",
+    author_email="wednesware@gmail.com",
+    description="Easy, ultra-light-weight installer for Wednesware publications. Built for single-use and multi-use installations.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/wednesware/nitrogen",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
