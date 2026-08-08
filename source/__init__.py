cd94df1a39e49fbc36d4f4e750859f51e43a094f
vesperyn <ves@wednesware.org>
2026-08-08 13:20:45 +0200

    26.2

--- source/__init__.py
+++ source/__init__.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from urllib.request import urlretrieve
 
 
+SOURCEGEN_VERSION: str = "26.2" # SHOULD NOT BE CHANGED
+
 NAME: str = "Sourcegen" # TODO
 DESCRIPTION: str = "Enter a short one-line description here." # TODO
 VERSION: str = "26.1" # TODO
